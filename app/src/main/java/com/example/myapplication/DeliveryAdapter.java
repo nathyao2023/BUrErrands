3eb0449d4819507e7b2c773b9f945757d50c2ad7
nathyao2023 <124156343+nathyao2023@users.noreply.github.com>
2023-05-02 03:02:12 -0400

    Revert "delivery confirm AlertDialog"
    This reverts commit ec63b5f79c6cb0ed300bd458eb2431e3839c6e94.

--- app/src/main/java/com/example/myapplication/DeliveryAdapter.java
+++ app/src/main/java/com/example/myapplication/DeliveryAdapter.java
@@ -2,17 +2,13 @@ package com.example.myapplication;
 
 
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+        import android.widget.Button;
+        import android.widget.Toast;
 
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.BaseViewHolder;
-import com.example.myapplication.R;
-import com.example.myapplication.Order;
-import android.app.AlertDialog;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.TextView;
+        import com.chad.library.adapter.base.BaseQuickAdapter;
+        import com.chad.library.adapter.base.BaseViewHolder;
+        import com.example.myapplication.R;
+        import com.example.myapplication.Order;
 
 import java.util.List;
 
@@ -29,6 +25,7 @@ public class DeliveryAdapter extends BaseQuickAdapter<Order, BaseViewHolder> {
     }
 
 
+
     @Override
     protected void convert(BaseViewHolder helper, Order item) {
         helper.setText(R.id.order_id, "Order ID: " + item.getOrderNumber())
@@ -39,45 +36,7 @@ public class DeliveryAdapter extends BaseQuickAdapter<Order, BaseViewHolder> {
         Button acceptOrderButton = helper.getView(R.id.accept_order_button);
         acceptOrderButton.setOnClickListener(v -> {
             // Perform actions when the accept order button is clicked
-            showOrderDetailsDialog(item);
+            Toast.makeText(helper.itemView.getContext(), "Order accepted: " + item.getOrderNumber(), Toast.LENGTH_SHORT).show();
         });
-    }
-
-    private void showOrderDetailsDialog(Order item) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-        View view = LayoutInflater.from(mContext).inflate(R.layout.delivery_accept_layout, null);
-        TextView orderDetailsText = view.findViewById(R.id.delivery_details_text);
-        Button closeButton = view.findViewById(R.id.close_button);
-
-        String orderDetails = "Order accepted:\n" +
-                "Order ID: " + item.getOrderNumber() + "\n" +
-                "Order Name: " + item.getOrderName() + "\n" +
-                "Start Address: " + item.getStartAddress() + "\n" +
-                "End Address: " + item.getEndAddress();
-
-        orderDetailsText.setText(orderDetails);
-
-        builder.setView(view);
-        AlertDialog dialog = builder.create();
-
-        closeButton.setOnClickListener(v -> {
-            dialog.dismiss();
-        });
-
-        dialog.show();
     }
 }
-
-
-
-
-//
-//    String toastMessage = "Order accepted:\n" +
-//                    "Order ID: " + item.getOrderNumber() + "\n" +
-//                    "Order Name: " + item.getOrderName() + "\n" +
-//                    "Start Address: " + item.getStartAddress() + "\n" +
-//                    "End Address: " + item.getEndAddress();
-//            Toast.makeText(mContext, toastMessage, Toast.LENGTH_LONG).show();
-//        });
-//    }
-//}
